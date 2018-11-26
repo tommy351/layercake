39e423b99747b83685e0ddc398f30f267475908a
Tommy Chen <tommy351@gmail.com>
2018-11-26 22:57:06 +0800

    Add version flag

--- main.go
+++ main.go
@@ -11,12 +11,18 @@ import (
 )
 
 type GlobalOptions struct {
-	Config string `long:"config" description:"Path to config file" value-name:"PATH"`
-	CWD    string `long:"cwd" description:"Set working directory" value-name:"PATH"`
-	Debug  bool   `long:"debug" description:"Enable debug mode"`
+	Config  string `long:"config" description:"Path to config file" value-name:"PATH"`
+	CWD     string `long:"cwd" description:"Set working directory" value-name:"PATH"`
+	Debug   bool   `long:"debug" description:"Enable debug mode"`
+	Version bool   `long:"version" short:"v" description:"Print version information"`
 }
 
-const layercakeBaseDir = ".layercake"
+const (
+	layercakeBaseDir = ".layercake"
+	version          = "dev"
+	commit           = "none"
+	date             = "unknown"
+)
 
 var (
 	globalOptions GlobalOptions
@@ -40,13 +46,21 @@ func main() {
 		if e, ok := err.(*flags.Error); ok {
 			switch e.Type {
 			case flags.ErrHelp, flags.ErrCommandRequired:
-				parser.WriteHelp(os.Stderr)
+				printHelp()
 				return
 			}
 		}
 
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
+	}
+}
+
+func printHelp() {
+	if globalOptions.Version {
+		fmt.Printf("%s, commit %s, built at %s", version, commit, date)
+	} else {
+		parser.WriteHelp(os.Stderr)
 	}
 }
 
